69481937a9ea149ac7cb1d4d4a5528b13e939e58
tongbing <tongbing@chuanglintech.com>
2023-04-20 09:59:50 +0800

    update CR13 variate type & add Path type & delete useless file

--- src/main/java/org/ldbcouncil/finbench/driver/result/Path.java
+++ src/main/java/org/ldbcouncil/finbench/driver/result/Path.java
@@ -1,4 +1,50 @@
 package org.ldbcouncil.finbench.driver.result;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+// The Type of path, [ID1, ID2, ID3, ID4...]
 public class Path {
+    private List<Long> path;
+
+    public Path(List<Long> path) {
+        this.path = path;
+    }
+
+    public Path() {
+        path = new ArrayList<>();
+    }
+
+    public void setPath(List<Long> path) {
+        this.path = path;
+    }
+
+    public void addId(Long id) {
+        this.path.add(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Path path1 = (Path) o;
+        return Objects.equals(path, path1.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path);
+    }
+
+    @Override
+    public String toString() {
+        return "Path{"
+            + "path=" + path
+            + '}';
+    }
 }
