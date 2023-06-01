6141d4c80bf8cf7800ef0b1ad69a03c6bab88911
tongbing <tongbing@chuanglintech.com>
2023-06-01 18:55:49 +0800

    update params index

--- src/main/java/org/ldbcouncil/finbench/driver/workloads/transaction/UpdateEventStreamReader.java
+++ src/main/java/org/ldbcouncil/finbench/driver/workloads/transaction/UpdateEventStreamReader.java
@@ -717,12 +717,12 @@ public class UpdateEventStreamReader implements Iterator<Operation> {
                 long dstId = rs.getLong(4);
                 Date currentTime = new Date(scheduledStartTimeAsMilli);
                 long amt = rs.getLong(5);
-                double amountThreshold = rs.getDouble(9);
-                Date startTime = new Date(rs.getLong(5));
-                Date endTime = new Date(rs.getLong(6));
-                float ratioThreshold = rs.getFloat(10);
-                int truncationLimit = rs.getInt(7);
-                TruncationOrder truncationOrder = TruncationOrder.valueOf(rs.getString(8));
+                double amountThreshold = rs.getDouble(10);
+                Date startTime = new Date(rs.getLong(6));
+                Date endTime = new Date(rs.getLong(7));
+                float ratioThreshold = rs.getFloat(11);
+                int truncationLimit = rs.getInt(8);
+                TruncationOrder truncationOrder = TruncationOrder.valueOf(rs.getString(9));
 
                 Operation operation = new ReadWrite2(
                     srcId,
