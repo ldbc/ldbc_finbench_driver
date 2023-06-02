1f05f237b355968dde6ed36514b613c5f236e60e
tongbing <tongbing@chuanglintech.com>
2023-06-03 00:37:47 +0800

    update params index

--- src/main/java/org/ldbcouncil/finbench/driver/workloads/transaction/UpdateEventStreamReader.java
+++ src/main/java/org/ldbcouncil/finbench/driver/workloads/transaction/UpdateEventStreamReader.java
@@ -682,8 +682,8 @@ public class UpdateEventStreamReader implements Iterator<Operation> {
                 long dstId = rs.getLong(4);
                 Date currentTime = new Date(scheduledStartTimeAsMilli);
                 long amt = rs.getLong(5);
-                Date startTime = new Date(rs.getLong(6));
-                Date endTime = new Date(rs.getLong(7));
+                Date startTime = new Date(rs.getLong(10));
+                Date endTime = new Date(rs.getLong(11));
 
                 Operation operation = new ReadWrite1(
                     srcId,
@@ -717,12 +717,12 @@ public class UpdateEventStreamReader implements Iterator<Operation> {
                 long dstId = rs.getLong(4);
                 Date currentTime = new Date(scheduledStartTimeAsMilli);
                 long amt = rs.getLong(5);
-                double amountThreshold = rs.getDouble(10);
-                Date startTime = new Date(rs.getLong(6));
-                Date endTime = new Date(rs.getLong(7));
-                float ratioThreshold = rs.getFloat(11);
-                int truncationLimit = rs.getInt(8);
-                TruncationOrder truncationOrder = TruncationOrder.valueOf(rs.getString(9));
+                double amountThreshold = rs.getDouble(14);
+                Date startTime = new Date(rs.getLong(10));
+                Date endTime = new Date(rs.getLong(11));
+                float ratioThreshold = rs.getFloat(15);
+                int truncationLimit = rs.getInt(12);
+                TruncationOrder truncationOrder = TruncationOrder.valueOf(rs.getString(13));
 
                 Operation operation = new ReadWrite2(
                     srcId,
@@ -759,11 +759,11 @@ public class UpdateEventStreamReader implements Iterator<Operation> {
                 long srcId = rs.getLong(3);
                 long dstId = rs.getLong(4);
                 Date currentTime = new Date(scheduledStartTimeAsMilli);
-                double threshold = rs.getDouble(9);
-                Date startTime = new Date(rs.getLong(5));
-                Date endTime = new Date(rs.getLong(6));
-                int truncationLimit = rs.getInt(7);
-                TruncationOrder truncationOrder = TruncationOrder.valueOf(rs.getString(8));
+                double threshold = rs.getDouble(10);
+                Date startTime = new Date(rs.getLong(6));
+                Date endTime = new Date(rs.getLong(7));
+                int truncationLimit = rs.getInt(8);
+                TruncationOrder truncationOrder = TruncationOrder.valueOf(rs.getString(9));
 
                 Operation operation = new ReadWrite3(
                     srcId,
